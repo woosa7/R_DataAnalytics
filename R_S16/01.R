ac8707a46c576b6907f63ebb7aa49a3f8aa484e8
woosa7 <woosa7@gmail.com>
2016-07-18 18:07:29 +0900

    20160718 R과 통계

--- R_S16/01.R
+++ R_S16/01.R
@@ -1,90 +1,143 @@
-setwd("c:/R_Study/R_Summer")
+################################################################
+#
+# R을 활용한 통계분석 : 정여진 교수 (2016 여름특강)
+#
+# 1. 경영통계 요약정리
+#
+################################################################
 
-install.packages("reshape")
+#---------------------------------------------------------------
+# 질적 자료 (범주형) : 명목척도 / 서열척도
+# 양적 자료 : 구간척도 (원점 없음) / 비율척도 (원점 존재)
+#---------------------------------------------------------------
+
+#---------------------------------------------------------------
+# 양적 자료의 요약
+#---------------------------------------------------------------
+
 library(reshape)
-attach(tips)
+
+attach(tips)    # dataframe name 호출하지 않아도 됨.
 summary(tips)
 
-# size를 범주형 자료로 처리하길 원하는 경우.
+library(psych)
+describe(tips)
+headTail(tips)
+
+# 양적자료인 size를 범주형 자료로 처리하길 원하는 경우.
 tips2 <- tips
 tips2$size = factor(tips2$size)
 summary(tips2)
 
-
-# median
-summary(tips$tip)
-
+# median : 극단값이 있는 경우 중심위치 측정에 선호
 tips2$tip[1]=100
-summary(tips2$tip)
-
 quantile(tips$tip)
 quantile(tips2$tip)
 
-
-# 표준편차 / 변동계수
-tip <- tips$tip
-
+# 분산 / 표준편차
 var(tip)
 sd(tip)
 
+# 변동계수(Coefficient of Variation) = 표준편차 / 평균
+# 표준편차가 평균에 비하여 얼마나 큰지 나타냄. 두 집단의 변동성 비교시 사용.
+# 평균이 클수록 표준편차가 큰 경향이 있기 때문에 표준편차로 변동성 비교할 수 없음.
 sd(tip) / mean(tip)
 
 
-# IQR
+# 사분위수 범위 (interquartile range, IQR)
+# Q1과 Q3의 차이. 자료의 중간 50%의 범위. 극단값에 상대적으로 덜 민감.
 IQR(tip)
 
+# plot
 boxplot(tip, col="red", horizontal = T, xlab="Tip")
-hist(tip, 20, probability = T)
 
+hist(tip)
+hist(tip, 20, probability = T, xlim=c(0,10), ylim = c(0,0.5))   # break : 20 / probability : 갯수가 아닌 확률로 표시
+lines(density(tip), col="blue")
+
+# Q-Q Normality Plot : 자료가 정규분포에 얼마나 근접한지 판단
 qqnorm(tip)
 qqline(tip) # 점들이 선위에 가까이 있을수로 정규분포를 따름.
 
 
-#-----------------------------
+
+#---------------------------------------------------------------
 # 질적자료의 요약
+#---------------------------------------------------------------
 
-tips$day <- factor(tips$day, levels = c("Thur","Fri","Sat","Sun"))
-mytable <- table(tips$day)
+# table() : 도수분포표
+# barplot()
+# pie()
 
-lbl <- paste(names(mytable), ",", round(mytable/sum(mytable)*100),"%", sep="")
+summary(day)
+barplot(table(day))
+
+day <- factor(day, levels = c("Thur","Fri","Sat","Sun"))   # 요일 순서대로 나오도록 factor levels 변경
+summary(day)
+mytable <- table(day)
 
 barplot(mytable)
+barplot(mytable / sum(mytable))
 
-pie(mytable, labels = lbl)
+lbl <- paste(names(mytable), ", ", round(mytable/sum(mytable)*100), "%", sep="")
+pie(mytable, labels = lbl, col = rainbow(length(mytable)), main = "Day of Tips")
 
 
+
+#---------------------------------------------------------------
 # 두 변수의 요약
+#---------------------------------------------------------------
+
+# 두 범주형 변수의 요약
+# xtabs(~그룹변수1 + 그룹변수2, data) : 분할표
 mytable2 <- xtabs(~sex+day, tips)
 mytable2
 
-barplot(mytable2, legend.text = c("Female", "Male"))
+barplot(mytable2, legend.text = c("Female", "Male"), ylim = c(0,100))
+barplot(mytable2, legend.text = c("Female", "Male"), ylim = c(0,80), beside = T)
 
-barplot(mytable2, legend.text = c("Female", "Male"), beside = T, ylim = c(0,80))
+barplot(xtabs(~sex+smoker, tips), legend.text = c("Female", "Male"), ylim = c(0,100), beside = T, xlab = "smoker")
+barplot(xtabs(~size+time, tips), legend.text = c(1,2,3,4,5,6), beside = T, xlab = "size - time")
 
-mosaicplot(mytable2)
-mosaicplot(t(mytable2))
+mosaicplot(mytable2)     # 성별 기준으로 요일별 비교
+mosaicplot(t(mytable2))  # 요일 기준으로 성별 비교
 
+# 범주형 변수와 양적 변수의 요약 
 boxplot(tip~day, data=tips, ylab="tips", xlab="day")
 
-
+# 두 양적변수의 요약
 plot(tip~total_bill, tips)
 
 
 
 #--------------------------------------------
-# exercise
+# practice
 
+kmovie <- read.csv("movie_utf8.csv",stringsAsFactors = F)
+head(kmovie)
+View(kmovie)
+str(kmovie)
+attach(kmovie)
 
-#-------------------
-DF <- read.csv("movie_MBA2.csv",stringsAsFactors = F)
-head(DF)
-View(DF)
-str(DF)
-boxplot(DF$total_seen)
+# 총관객수(total_seen)에 대해 평균, 중위수, 분산, 표준편차, 사분위수, 사분위수범위
+mean(total_seen)
+median(total_seen)
+
+var(total_seen)
+sd(total_seen)
+
+quantile(total_seen)
+summary(total_seen)
+IQR(total_seen)
+
+boxplot(total_seen/1000, horizontal=T)
+hist(total_seen/1000)
+# ---> 우측에 outlier가 너무 많다. 적당히 변환 후 분석 진행 필요 
+
 unique(DF$rating)
-agrating <- aggregate(total_seen~rating,data = DF,mean)
+agrating <- aggregate(total_seen~rating, data = kmovie, mean)
 agrating
-boxplot(total_seen~rating,data=DF, ylab="tips",xlab="day")
+boxplot(total_seen~rating, data=kmovie, ylab="tips", xlab="day")
 
 tableOfRating <- table(DF$rating)
 tableOfRating
