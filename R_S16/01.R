4361b50f06269b1002714544a9161c426f36c1ad
woosa7 <woosa7@gmail.com>
2016-07-22 18:17:58 +0900

    20160722 여름특강 정리

--- R_S16/01.R
+++ R_S16/01.R
@@ -30,7 +30,7 @@ tips2$size = factor(tips2$size)
 summary(tips2)
 
 # median : 극단값이 있는 경우 중심위치 측정에 선호
-tips2$tip[1]=100
+tips2$tip[1] = 100
 quantile(tips$tip)
 quantile(tips2$tip)
 
@@ -43,22 +43,20 @@ sd(tip)
 # 평균이 클수록 표준편차가 큰 경향이 있기 때문에 표준편차로 변동성 비교할 수 없음.
 sd(tip) / mean(tip)
 
-
 # 사분위수 범위 (interquartile range, IQR)
 # Q1과 Q3의 차이. 자료의 중간 50%의 범위. 극단값에 상대적으로 덜 민감.
 IQR(tip)
 
 # plot
-boxplot(tip, col="red", horizontal = T, xlab="Tip")
+boxplot(tip, col = "red", horizontal = T, xlab = "Tip")
 
 hist(tip)
-hist(tip, 20, probability = T, xlim=c(0,10), ylim = c(0,0.5))   # break : 20 / probability : 갯수가 아닌 확률로 표시
+hist(tip, 20, probability = T, xlim = c(0,10), ylim = c(0,0.5))   # break : 20 / probability : 갯수가 아닌 확률로 표시
 lines(density(tip), col="blue")
 
 # Q-Q Normality Plot : 자료가 정규분포에 얼마나 근접한지 판단
 qqnorm(tip)
 qqline(tip) # 점들이 선위에 가까이 있을수로 정규분포를 따름.
-
 
 
 #---------------------------------------------------------------
@@ -72,9 +70,10 @@ qqline(tip) # 점들이 선위에 가까이 있을수로 정규분포를 따름.
 summary(day)
 barplot(table(day))
 
-day <- factor(day, levels = c("Thur","Fri","Sat","Sun"))   # 요일 순서대로 나오도록 factor levels 변경
+tips$day <- factor(day, levels = c("Thur","Fri","Sat","Sun"))   # 요일 순서대로 나오도록 factor levels 변경
 summary(day)
 mytable <- table(day)
+mytable
 
 barplot(mytable)
 barplot(mytable / sum(mytable))
@@ -83,13 +82,14 @@ lbl <- paste(names(mytable), ", ", round(mytable/sum(mytable)*100), "%", sep="")
 pie(mytable, labels = lbl, col = rainbow(length(mytable)), main = "Day of Tips")
 
 
-
 #---------------------------------------------------------------
 # 두 변수의 요약
 #---------------------------------------------------------------
 
 # 두 범주형 변수의 요약
 # xtabs(~그룹변수1 + 그룹변수2, data) : 분할표
+
+# 성별~요일별
 mytable2 <- xtabs(~sex+day, tips)
 mytable2
 
@@ -105,67 +105,73 @@ mosaicplot(t(mytable2))  # 요일 기준으로 성별 비교
 # 범주형 변수와 양적 변수의 요약 
 boxplot(tip~day, data=tips, ylab="tips", xlab="day")
 
-# 두 양적변수의 요약
+# 두 양적변수의 요약 (주문금액 대비 팁)
 plot(tip~total_bill, tips)
-
 
 
 #--------------------------------------------
 # practice
+#--------------------------------------------
 
-kmovie <- read.csv("movie_utf8.csv",stringsAsFactors = F)
-head(kmovie)
+kmovie <- read.csv("movie_utf8.csv", stringsAsFactors = F)
 View(kmovie)
 str(kmovie)
+
+quantile(kmovie$total_seen)
+kmovie$total_seen <- kmovie$total_seen/1000
+
+quantile(kmovie$total_sales)
+kmovie$total_sales <- kmovie$total_sales/1000000
+
 attach(kmovie)
 
-# 총관객수(total_seen)에 대해 평균, 중위수, 분산, 표준편차, 사분위수, 사분위수범위
+#--------------------------------------------
+# 1. 총관객수(total_seen)에 대해 통계량
+
 mean(total_seen)
 median(total_seen)
-
-var(total_seen)
 sd(total_seen)
 
 quantile(total_seen)
 summary(total_seen)
 IQR(total_seen)
 
-boxplot(total_seen/1000, horizontal=T)
-hist(total_seen/1000)
-# ---> 우측에 outlier가 너무 많다. 적당히 변환 후 분석 진행 필요 
+boxplot(total_seen, horizontal=T)
+hist(total_seen, 20)   # ---> 우측에 outlier가 너무 많다. 적당히 변환 후 분석 진행 필요 
 
-unique(DF$rating)
-agrating <- aggregate(total_seen~rating, data = kmovie, mean)
-agrating
-boxplot(total_seen~rating, data=kmovie, ylab="tips", xlab="day")
+#--------------------------------------------
+# 2. 등급별 평균 매출액
 
-tableOfRating <- table(DF$rating)
-tableOfRating
-barplot(tableOfRating,ylab="CNT",xlab="Rating")
-pie(tableOfRating)
-lbl2 <- paste(names(tableOfRating),",",round(prop.table(tableOfRating)* 100,2) ,"%",sep="")
-pie(tableOfRating,labels = lbl2)
-
-
-
-#-----------------------------------------
-movie <- read.csv("movie_MBA.csv")
-head(movie)
-
-boxplot(total, horizontal = T, ylim=c(0,13000))
-hist(total,20)
-
-
-par(las=2, mar=c(10,5,5,5))
-boxplot(total_seen~rating, movie)
+unique(rating)
+aggregate(total_sales~rating, data = kmovie, mean)
+par(las = 2, mar = c(10,5,5,5))   
+boxplot(total_sales~rating, data = kmovie, ylab="sales (x1,000,000)", xlab="rating")
+boxplot(log(total_sales)~rating, data = kmovie, ylab="sales : log(x1,000,000)", xlab="rating")
 
 library(plyr)
-msales <- ddply(movie, ~rating, summarise, mean_sales=mean(total_sales))
+msales <- ddply(kmovie, ~rating, summarise, mean_sales=mean(total_sales))
+msales
 barplot(msales[,2], names.arg = msales[,1])
-tab=xtabs(~genre+rating, movie)
 
+#--------------------------------------------
+# 3. 등급별 개봉 영화 수 : barplot & pie chart
+tableOfRating <- table(rating)
+tableOfRating
 
-par(las=2)
+# las : style of axis labels. mar : plot margin, c(bottom, left, top, right)
+bp <- barplot(tableOfRating, ylab="Count", xlab="Rating", ylim = c(0,100), las = 2, mar = c(10,5,5,5))
+text(x = bp, y = tableOfRating*0.9, labels = tableOfRating, col = "red", cex = 0.9)
+
+lbl2 <- paste(names(tableOfRating), "\n", round(prop.table(tableOfRating)* 100,2), "%", sep="")
+pie(tableOfRating, labels = lbl2, col = rainbow(4))
+
+#--------------------------------------------
+# 4. 등급과 장르 사용해 분할표 생성. bar plot과 mosaic plot 그리기.
+
+tab = xtabs(~genre+rating, kmovie)
+tab
+
+barplot(tab, legend.text=row.names(tab), col = rainbow(9), beside=T, ylim = c(0,40))
+
 mosaicplot(tab)
 mosaicplot(t(tab))
-
