ae378d14e0c9438117af98b4d567f21c9802d790
woosa7 <woosa7@gmail.com>
2016-08-01 18:27:43 +0900

    분산분석 (ANOVA), 공분산분석 (ANCOVA)

--- CommonSetting.R
+++ CommonSetting.R
@@ -4,10 +4,12 @@ setwd("c:/R_Study/R_KMU")
 setwd("/Volumes/MacHDD/workspace/R_Study/R_S16")
 
 windows(800, 600, pointsize = 12)   # 별도의 윈도우 열기
-savePlot("aa.png", type="png")    # 결과물을 그림으로 저장
+savePlot("aa.png", type="png")      # 결과물을 그림으로 저장
 dev.off()                           # 윈도우 닫기
 
 install.packages("readxl")
+
+colors()   # color name list
 
 
 ##################################
@@ -18,16 +20,26 @@ install.packages("readxl")
 # bestprediction <- dataframe[ , mask]
 # lm(targetVariables ~ bestprediction)
 
+
+# 특정 컬럼을 기준으로 회귀분석을 한꺼번에 진행.
 library(MASS)
 attach(Cars93)
+head(Cars93)
 
 tapply(Weight, Origin, mean)
 tapply(Weight, Origin, length)
 
-model <- by(Cars93, Origin, function(df) lm(Price~Weight+EngineSize+Horsepower, data = df))
+# Origin 기준으로 
+model <- by(Cars93, Origin, function(df) lm(MPG.highway ~ Weight + EngineSize + Horsepower, data = df))
 model
-
 summary(model$USA)
 summary(model$`non-USA`)
+
+# Type 기준으로
+model <- by(Cars93, Type, function(df) lm(MPG.city ~ Weight + EngineSize + Horsepower, data = df))
+model
+summary(model$Small)
+summary(model$Large)
+
 
 ##################################
