696f377e5853fc7936f62265c76403613a8e6f9b
woosa7 <woosa7@gmail.com>
2016-10-05 18:32:47 +0900

    Customer Signature

--- R_DataMining/cutomer_signature.R
+++ R_DataMining/cutomer_signature.R
@@ -85,24 +85,79 @@ totalAmt <- tr %>%
     group_by(custid) %>% summarise(PAmount = sum(net_amt)) %>% 
     mutate(PAmount = ifelse(PAmount < 0, 0, PAmount))
 
+# price_group : 선호 가격대 그룹
+cutRange <- c(-1, 100000, 200000, 300000, 400000, 500000, 1000000, 10000000)
+cutLabel <- c(1, 10, 20, 30, 40, 50, 100)
+
 cs.v13 <- inner_join(cs.v3, totalAmt) %>% mutate(PAPV = PAmount / visits) %>%
     mutate(P_group = ifelse(PAPV >= mean(PAPV) & API <= mean(API), 1, 99)) %>%
     mutate(P_group = ifelse(PAPV >= mean(PAPV) & API > mean(API), 2, P_group)) %>%
     mutate(P_group = ifelse(PAPV < mean(PAPV) & API <= mean(API), 3, P_group)) %>%
     mutate(P_group = ifelse(PAPV < mean(PAPV) & API > mean(API), 4, P_group)) %>%
     mutate(PAPV = floor(PAmount / visits / 10000) * 10000) %>%                      # 10,000원 단위
-    select(custid, PAPV, API)
+    mutate(price_group = cut(PAPV, cutRange, labels = cutLabel)) %>%
+    select(custid, PAPV, P_group, price_group)
 
 cs.v13
 
 cs.v13 %>% group_by(P_group) %>% summarise(p = n())
+cs.v13 %>% group_by(price_group) %>% summarise(p = n())
+cs.v13 %>% filter(price_group == 10)
 
 
 #-----------------------------------------------------------------
 # 구매추세 패턴
+# 전반기/후반기 6개월간의 평균구매주기(API)를 비교하여 상승, 하강, 유지, 신규, 이탈로 구분
+
+cs.v14 <- cs %>% select(custid)
+
+# 전반기 API
+start_date <- ymd(ymd_hms(min(tr$sales_date)))
+end_date = start_date + months(6)
+
+temp1 <- tr %>% 
+    filter(sales_date >= start_date & sales_date < end_date) %>% 
+    distinct(custid, sales_date) %>% 
+    group_by(custid) %>% summarize(visits1 = n()) %>% 
+    mutate(api1 = as.integer((end_date - start_date) / visits1)) %>%
+    select(custid, api1)
+
+temp1
+
+# 후반기 API
+start_date <- end_date
+end_date = start_date + months(6)
+
+temp2 <- tr %>% 
+    filter(sales_date >= start_date & sales_date < end_date) %>% 
+    distinct(custid, sales_date) %>% 
+    group_by(custid) %>% summarize(visits2 = n()) %>% 
+    mutate(api2 = as.integer((end_date - start_date) / visits2)) %>%
+    select(custid, api2)
+
+temp2
+
+# 후반기 > 전반기 * 1.5 : up
+# 전반기 > 후반기 * 1.5 : down
+# 전반기 = 0 : new / 후반기 = 0 : leave
+cs.v14 <- left_join(cs.v14, temp1)  %>% left_join(temp2) %>%
+    mutate(api1 = ifelse(is.na(api1), 0, api1),
+           api2 = ifelse(is.na(api2), 0, api2)) %>%
+    mutate(p_trend = ifelse(api2 > api1 * 1.5, "up", "keep"),
+           p_trend = ifelse(api1 > api2 * 1.5, "down", p_trend),
+           p_trend = ifelse(api1 == 0, "new", p_trend),
+           p_trend = ifelse(api2 == 0, "leave", p_trend)) %>%
+    select(custid, p_trend, api1, api2)
+
+cs.v14
+
+cs.v14 %>% group_by(p_trend) %>% summarise(p = n())
+
 
 #-----------------------------------------------------------------
 # 상품별 구매금액 및 횟수
+
+
 
 #-----------------------------------------------------------------
 # 상품별 구매순서
