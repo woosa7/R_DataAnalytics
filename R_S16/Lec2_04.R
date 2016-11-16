f0d75d1d4e0f10fbb6ff74e5fd4ff13d7dba5af8
Woo-Hyun Kim <woosa7@gmail.com>
2016-11-16 22:17:05 +0900

    Cluster analysis - Practice4

--- R_S16/Lec2_04.R
+++ R_S16/Lec2_04.R
@@ -209,14 +209,26 @@ plot(mc4)
 #--------------------------------------------------------------
 # Jet
 
+# 22개 미국 전투기
+# -	FFD: 처음 비행 날짜
+# -	CAR: 비행기가 항공모함에 착륙 가능여부
+# -	SPR: 단위무게 당 출력에 비례하는 특정한 출력
+# -	RGF: 비행범위 요인
+# -	PLF: 비행기의 총 무게의 일부분으로서의 탑재량
+# -	SLF: 일관된 무게 요인
+
+#--------------------------------------------------------------
+# 1. 계층적군집분석
+
+# A. FFD와 CAR를 제외한 변수를 표준화 한 후 계층적 군집화를 시행하고 덴드로그램을 그리시오.
+
 jet = read.csv("jet.csv", header = T)
-jet
 
 rownames(jet) = jet$X
-jet = jet[, -c(1,7)]
-jet_s = scale(jet)
+jet = jet[, -c(1,2,7)]   # FFD, CAR 제외.
+jet
 
-distJet = dist(jet_s)
+distJet = dist(scale(jet))
 
 par(mfcol=c(2,2))
 hc1 = hclust(distJet, method = "single")
@@ -229,8 +241,72 @@ hc4 = hclust(distJet, method = "ward.D")
 plot(hc4)
 par(mfcol=c(1,1))
 
-# complete 선택
+
+# B. A의 결과를 사용해 두 개의 집단으로 관측치를 분류하고 각 집단의 특징을 원변수 관점에서 비교하시오.
+
+# 2개의 군집으로 나누기 좋은 complete linkage 선택
+result = cutree(hc2, k=2)
+
+par(mfrow=c(2,3))
+
+plot(jet$SPR, jet$RGF, col=result, pch=result, xlab = "SPR", ylab = "RGF")
+text(jet$SPR, jet$RGF, labels = rownames(jet), col=result, pos = 1)
+abline(v = 3.7)
+
+plot(jet$SPR, jet$PLF, col=result, pch=result, xlab = "SPR", ylab = "PLF")
+text(jet$SPR, jet$PLF, labels = rownames(jet), col=result, pos = 1)
+abline(v = 3.7)
+
+plot(jet$SPR, jet$SLF, col=result, pch=result, xlab = "SPR", ylab = "SLF")
+text(jet$SPR, jet$SLF, labels = rownames(jet), col=result, pos = 1)
+abline(v = 3.7)
+
+plot(jet$RGF, jet$PLF, col=result, pch=result, xlab = "RGF", ylab = "PLF")
+text(jet$RGF, jet$PLF, labels = rownames(jet), col=result, pos = 1)
+
+plot(jet$RGF, jet$SLF, col=result, pch=result, xlab = "RGF", ylab = "SLF")
+text(jet$RGF, jet$SLF, labels = rownames(jet), col=result, pos = 1)
+
+plot(jet$PLF, jet$SLF, col=result, pch=result, xlab = "PLF", ylab = "SLF")
+text(jet$PLF, jet$SLF, labels = rownames(jet), col=result, pos = 1)
+
+par(mfcol=c(1,1))
+
+
+# ward.D
 result = cutree(hc4, k=2)
+
+par(mfrow=c(2,3))
+
+plot(jet$SPR, jet$RGF, col=result, pch=result, xlab = "SPR", ylab = "RGF")
+text(jet$SPR, jet$RGF, labels = rownames(jet), col=result, pos = 1)
+
+plot(jet$SPR, jet$PLF, col=result, pch=result, xlab = "SPR", ylab = "PLF")
+text(jet$SPR, jet$PLF, labels = rownames(jet), col=result, pos = 1)
+
+plot(jet$SPR, jet$SLF, col=result, pch=result, xlab = "SPR", ylab = "SLF")
+text(jet$SPR, jet$SLF, labels = rownames(jet), col=result, pos = 1)
+abline(h = 1.5)
+
+plot(jet$RGF, jet$PLF, col=result, pch=result, xlab = "RGF", ylab = "PLF")
+text(jet$RGF, jet$PLF, labels = rownames(jet), col=result, pos = 1)
+
+plot(jet$RGF, jet$SLF, col=result, pch=result, xlab = "RGF", ylab = "SLF")
+text(jet$RGF, jet$SLF, labels = rownames(jet), col=result, pos = 1)
+abline(h = 1.5)
+
+plot(jet$PLF, jet$SLF, col=result, pch=result, xlab = "PLF", ylab = "SLF")
+text(jet$PLF, jet$SLF, labels = rownames(jet), col=result, pos = 1)
+abline(h = 1.5)
+
+par(mfcol=c(1,1))
+
+
+jet
+
+
+# C. 두 집단을 주성분을 이용해 2차원 산점도로 표현하시오. 
+# (즉, 제1 주성분과 제2 주성분을 사용한 산점도에서 두 개의 집단을 서로 다른 마크와 색으로 표현하시오.)
 
 # 1
 par(mfcol=c(2,3))
@@ -266,6 +342,10 @@ summary(mc)
 
 
 
+# 2.	비계층적군집분석
+# A.	군집 개수 1~5까지를 사용해 k-means clustering을 시행하고 얻은 within-group sum of squares를 저장하고 그래프로 표현하여 적절한 군집 개수를 판단하시오.
+# B.	K-means clustering을 이용해 2개의 집단으로 군집화하고 그 결과를 1번의 B, C와 같이 탐색하시오.
+# 3.	모형기반 군집화를 통해 최적의 군집 개수를 찾고 그 결과를 1번의 B, C와 같이 탐색하시오.
 
 
 
