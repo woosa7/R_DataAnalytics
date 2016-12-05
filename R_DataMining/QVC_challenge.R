b7be5a259071758bed680ecb512684563f9c3b1e
woosa7 <woosa7@woosa7ui-MacBook-Pro.local>
2016-12-05 18:06:48 +0900

    QVC - ARM

--- R_DataMining/QVC_challenge.R
+++ R_DataMining/QVC_challenge.R
@@ -14,14 +14,68 @@ airtime = read.csv("QVC/airtime.csv", header = T, stringsAsFactors = F)
 order = read.csv("QVC/order.csv", header = T, stringsAsFactors = F)
 
 head(customer)
+head(order)
 summary(customer)
 
 
+#--------------------------------------------------------------------
+# Sequence rule analysis
+#--------------------------------------------------------------------
+
+install.packages("arulesSequences")
+library(arulesSequences)
+library(dplyr)
+
+head(order)
+dim(order)
+
+# ORDER_DATE = "sequenceID", CUSTOMER_NBR = "eventID", "SIZE", PRODUCT_ID = items
+makeSeqData <- function(x) {
+    seqData = NULL
+    sequenceID = 1
+    for (i in 1:nrow(x)) {
+        tx = x[i,]
+        basket = order %>% filter(CUSTOMER_NBR == tx$CUSTOMER_NBR, ORDER_NBR == tx$ORDER_NBR) %>% 
+            distinct(PRODUCT_ID)
+        items = paste(basket$PRODUCT_ID, collapse = ",")
+        
+        custTr = order %>% filter(CUSTOMER_NBR == tx$CUSTOMER_NBR, ORDER_NBR == tx$ORDER_NBR) %>% 
+            distinct(CUSTOMER_NBR, ORDER_NBR) %>% 
+            mutate(sequenceID = sequenceID, eventID = CUSTOMER_NBR, SIZE = nrow(basket), items = items) %>% 
+            select(sequenceID, eventID, SIZE, items)
+        seqData <- rbind(seqData, custTr)
+        
+        sequenceID = sequenceID + 1
+    }
+    return(seqData)
+}
+
+temp = order %>% distinct(CUSTOMER_NBR, ORDER_NBR)
+temp = temp[1:30, ]
+temp
+#filter(CUSTOMER_NBR == 355768, ORDER_NBR == 210) %>% 
+seqData = makeSeqData(temp)
+seqData
+
+head(seqData, 30)
+
+write.csv(seqData, "QVC/seqData.csv", row.names = F)
+
+write.table(seqData,"QVC/seqData.txt", sep="\t", row.names=FALSE)
+
+seqData %>% filter(sequenceID == 23)
 
 
+#--------------------------------------------------------------------
 
+x <- read_baskets(con  = "QVC/seqData.txt", sep = "\t", info = c("sequenceID","eventID","SIZE"))
+head(as(x, "data.frame"), 30)
 
+seq_rule_1 <- cspade(x, parameter = list(support = 0.3, maxsize = 5, maxlen = 4), control= list(verbose = TRUE))
+summary(seq_rule_1)
 
+as(zaki, "data.frame")
 
+data(zaki)
 
 
