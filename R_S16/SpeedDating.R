a7a327f1fc7714c34c912ee7f963127c59e492dd
woosa7 <woosa7@woosa7ui-MacBook-Pro.local>
2016-12-12 11:14:54 +0900

    다변량 기말과제

--- R_S16/SpeedDating.R
+++ R_S16/SpeedDating.R
@@ -16,7 +16,8 @@ head(oData)
 # iid = wave + id + gender : 1명의 참가자
 # 각 참가자별로 1 row. 6가지 항목의 값은 개인별로 동일.
 # 결측치 제거
-mainData = select(oData, iid, gender, attr1_1, sinc1_1, intel1_1, fun1_1, amb1_1, shar1_1, attr1_2, sinc1_2, intel1_2, fun1_2, amb1_2, shar1_2) %>% 
+mainData = oData %>% 
+    select(iid, gender, attr1_1, sinc1_1, intel1_1, fun1_1, amb1_1, shar1_1, attr1_2, sinc1_2, intel1_2, fun1_2, amb1_2, shar1_2) %>% 
     distinct(iid, gender, attr1_1, sinc1_1, intel1_1, fun1_1, amb1_1, shar1_1, attr1_2, sinc1_2, intel1_2, fun1_2, amb1_2, shar1_2) %>% 
     filter(!is.na(attr1_1), !is.na(attr1_2), !is.na(shar1_1))
 
@@ -26,6 +27,20 @@ rownames(mainData) = mainData$iid
 head(mainData, 20)
 summary(mainData)
 
+# 이성에게 선택된 횟수
+matchedData = oData %>% select(pid, match, partner, round) %>% 
+    group_by(pid, round) %>% summarise(matchedCnt = sum(match)) %>% 
+    mutate(ratio = matchedCnt/round, isHot = ifelse(ratio >= 0.4, 1, 0)) %>% 
+    select(pid, ratio, isHot)
+    #arrange(desc(ratio))
+
+head(matchedData)
+
+# bind data
+mainData = mainData %>% left_join(matchedData, by = c("iid" = "pid"))
+
+hotPeople = mainData %>% filter(isHot == 1)
+nrow(hotPeople)
 
 
 write.csv(mainData, "data/SpeedDatingMainData.csv", row.names = F)
@@ -56,8 +71,12 @@ pairs.panels(female_2)
 pairs.panels(male_2)
 
 # heatmap
-heatmap(as.matrix(all_1[,-1]), scale = "column", Colv = NA)
-heatmap(as.matrix(all_2[,-1]), scale = "column", Colv = NA)
+heatmap(as.matrix(all_1[,-1]), scale = "column", Colv = NA, main = "Before Dating")
+heatmap(as.matrix(all_2[,-1]), scale = "column", Colv = NA, main = "After Dating")
+
+# hotPeople
+hotPeople_1 = mainData[mainData$isHot==1, ][,c(2,3:8)]
+hotPeople_2 = mainData[mainData$isHot==1, ][,c(2,9:14)]
 
 
 
@@ -82,3 +101,23 @@ pca_all_2
 autoplot(pca_all_2, data = all_2, colour = 'gender', shape = F, label = T, loadings = T, 
          label.size = 7, loadings.label.size = 5, main = "PCA / After Dating",
          loadings.label = T, loadings.colour = "blue", loadings.label.colour = "blue")
+
+
+pca_hot_1 = prcomp(hotPeople_1[,-1], scale = T)
+summary(pca_hot_1)
+pca_hot_1
+
+autoplot(pca_hot_1, data = hotPeople_1, colour = 'gender', shape = F, label = T, loadings = T, 
+         label.size = 7, loadings.label.size = 5, main = "PCA / Before Dating (Hot People)",
+         loadings.label = T, loadings.colour = "blue", loadings.label.colour = "blue")
+
+
+pca_hot_2 = prcomp(hotPeople_2[,-1], scale = T)
+summary(pca_hot_2)
+pca_hot_2
+
+autoplot(pca_hot_2, data = hotPeople_2, colour = 'gender', shape = F, label = T, loadings = T, 
+         label.size = 7, loadings.label.size = 5, main = "PCA / After Dating (Hot People)",
+         loadings.label = T, loadings.colour = "blue", loadings.label.colour = "blue")
+
+
