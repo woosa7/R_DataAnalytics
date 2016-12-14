b987e96b48c20e9e9ad09c4d2c428f0ba5b06b82
woosa7 <woosa7@woosa7ui-MacBook-Pro.local>
2016-12-14 15:43:25 +0900

    dplyr / purrr - join data

--- R_JS/DataCamp04.R
+++ R_JS/DataCamp04.R
@@ -91,8 +91,6 @@ filter(hflights, DepDelay > 0 & ArrDelay < 0)       # departed late but arrived 
 
 filter(hflights, DepDelay > 0, Cancelled == 1)      # cancelled after being delayed
 
-
-# recap
 jfk = filter(fdata, Dest == "JFK") %>% 
     mutate(Date = paste(Year, Month, DayofMonth, sep = "-")) %>% 
     select(Date, DepTime, ArrTime, TailNum, Dest)
@@ -197,3 +195,168 @@ my_db <- src_mysql(dbname = "dplyr",
 
 nycflights <- tbl(my_db, "dplyr")
 
+
+#----------------------------------------------
+# Join
+#----------------------------------------------
+
+left_join()
+right_join()
+inner_join()
+full_join()
+
+semi_join(x, y, by)   # y와 match되는 x의 모든 row. x의 컬럼만 유지.
+anti_join(x, y, by)   # y에 없는 x의 모든 row. x의 컬럼만 유지.
+
+by = "key"
+by = c("first", "last")
+
+
+# x, y에 모두 name 컬럼이 있는데, 서로 다른 값인 경우.
+x %>% left_join(y, by = "key") %>% 
+    rename(artist = name.x, studio = name.y)
+
+
+# mismatched key names
+# x, y의 키 컬럼명이 다른 경우
+x %>% left_join(y, by = c("movie" = "name")) %>% 
+    rename(artist = name)
+
+
+# join multi tables
+library(purrr)
+
+list(x, y, z) %>% reduce(left_join)
+
+list(x, y, z) %>% reduce(semi_join)   # x의 row만 출력
+
+
+
+#----------------------------------------------
+# Practice - Lahman Baseball 
+#----------------------------------------------
+
+library(Lahman)
+
+str(Master)
+str(Salaries)
+str(Appearances)
+str(Batting)
+str(HallOfFame)
+
+players <- Master %>% 
+    select(playerID, nameFirst, nameLast) %>% 
+    distinct()
+
+head(players)
+
+#----------------------------------------------
+# Find all players who do not appear in Salaries
+players %>% 
+    anti_join(Salaries, by = "playerID") %>%
+    count()
+
+#----------------------------------------------
+# How many unsalaried players appear in Appearances?
+players %>% 
+    anti_join(Salaries, by = "playerID") %>% 
+    semi_join(Appearances, by = "playerID") %>% 
+    count()
+
+#----------------------------------------------
+# how many games each of these unsalaried players played?
+players %>% 
+    anti_join(Salaries, by = "playerID") %>% 
+    left_join(Appearances, by = "playerID") %>% 
+    group_by(playerID) %>%
+    summarise(total_games = sum(G_all)) %>%
+    arrange(desc(total_games))
+
+#----------------------------------------------
+# determine if the players had an at-bat (i.e. batted) in the games that they appeared in.
+players %>%
+    anti_join(Salaries, by = "playerID") %>% 
+    left_join(Batting, by = "playerID") %>% 
+    group_by(playerID, nameFirst, nameLast) %>%
+    summarise(total_games = sum(AB)) %>%
+    arrange(desc(total_games))
+
+#----------------------------------------------
+# Find the distinct players that appear in HallOfFame
+nominated <- HallOfFame %>% distinct(playerID)
+
+nominated_full <- nominated %>% 
+    left_join(Master, by = "playerID") %>% 
+    filter(inducted == "Y") %>% 
+    select(playerID, nameFirst, nameLast)
+
+head(nominated_full)
+
+
+#----------------------------------------------
+# Awards
+# Did nominees who were inducted earn more awards than nominees who were not inducted?
+
+# Hall of fame inductions
+inducted <- HallOfFame %>% 
+    filter(inducted == "Y") %>% 
+    distinct(playerID)
+
+head(inducted)
+
+# Tally the number of awards in AwardsPlayers by playerID
+nAwards <- AwardsPlayers %>% 
+    group_by(playerID) %>% 
+    tally()
+
+nAwards
+
+# players in inducted 
+nAwards %>% 
+    semi_join(inducted, by = "playerID") %>% 
+    summarize(avg_n = mean(n, na.rm = TRUE))
+
+# players in nominated, NOT in inducted 
+nAwards %>% 
+    semi_join(nominated, by = "playerID") %>%
+    anti_join(inducted, by = "playerID") %>% 
+    summarize(avg_n = mean(n, na.rm = TRUE))
+
+
+#----------------------------------------------
+# Salary
+# Does the maximum salary earned by inductees tend to be 
+# greater than the maximum salary earned by nominees who were not inducted?
+
+# Find the players who are in nominated, but not inducted
+notInducted <- nominated %>% 
+    setdiff(inducted)
+
+# not Inducted
+Salaries %>% 
+    semi_join(notInducted, by = "playerID") %>%
+    group_by(playerID) %>% 
+    summarize(max_salary = max(salary, na.rm = TRUE)) %>% 
+    summarize(avg_salary = mean(max_salary, na.rm = TRUE))
+
+# inducted
+Salaries %>% 
+    semi_join(inducted, by = "playerID") %>% 
+    group_by(playerID) %>% 
+    summarize(max_salary = max(salary, na.rm = TRUE)) %>% 
+    summarize(avg_salary = mean(max_salary, na.rm = TRUE))
+
+
+#----------------------------------------------
+# Retirement
+# players cannot be nominated until five years after they retire. 
+# Is this reflected in our data?
+
+nominated <- HallOfFame %>% distinct(playerID)
+
+Appearances %>% 
+    semi_join(nominated, by = "playerID") %>% 
+    group_by(playerID) %>% 
+    summarize(last_year = max(yearID)) %>% 
+    left_join(HallOfFame, by = "playerID") %>% 
+    filter(last_year >= yearID)
