d343c66c13e228ce0d7c15062dd4439f3908de66
Woo-Hyun Kim <woosa7@gmail.com>
2017-02-12 13:19:26 +0900

    RDSA 10

--- Acorn_R_Data/Chapter 10.R
+++ Acorn_R_Data/Chapter 10.R
@@ -13,6 +13,21 @@ b <- insert_top(a, c("o"))
 c <- without_top(a)
 d <- peek_top(a)
 
+
+## Fast Fully Persistent Queues ##
+a <- as.rpqueue(c("p","q","r","s")) 
+
+a$l
+a$r
+a$lhat
+
+b <- insert_back(a, "t")
+c <- without_front(b)
+d <- insert_back(c,"v")
+e <- insert_back(d,"w")
+f <- without_front(e)
+
+
 ## Slowly Pesistent Queues and Dequeues ##
 a <- as.rdeque(c("p", "q", "r","s","t","u","v"))
 b <- insert_front(a, c("o"))
@@ -20,11 +35,4 @@ c <- insert_back(a, c("w"))
 d <- without_front(a)
 e <- without_back(a)
 
-## Fast Fully Persistent Queues ##
-a <- as.rpqueue(c("p","q","r","s")) 
-b <- insert_back(a, "t")
-c <- without_front(b)
-d <- insert_back(c,"v")
-e <- insert_back(d,"w")
-f <- without_front(e)
 
